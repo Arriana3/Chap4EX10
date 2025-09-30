09bf9edc31b24dfe1df0b43f0591b9d0aff1fb26
Arriana3 <amm13857@email.vccs.edu>
2025-09-30 13:24:02 -0400

    Add project files.

---
+++ Chap4EX10.cpp
@@ -0,0 +1,23 @@
+/*File name: Chap4EX10.cpp
+* Programmer: Arriana Maldonado
+* Purpose: ask the user to enter the mouth 1-12 and the year
+then display the number of days in that month
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int month, year;
+
+	cout << "Enter a month (1-12): ";
+		cin >> month;
+	cout << "Enter a year: ";
+		cin >> year;
+
+		if (year % 100) {
+			if (year % 400)
+		
+
+
+	return 0;
+}
