04755f38e2d4e74b23f567736073e7e56b29c8be
Arriana3 <amm13857@email.vccs.edu>
2025-10-03 18:24:55 -0400

    step one

--- Chap4EX10.cpp
+++ Chap4EX10.cpp
@@ -13,11 +13,29 @@ int main()
 		cin >> month;
 	cout << "Enter a year: ";
 		cin >> year;
+		//gotta put all the months
+		if (year % 4 != 0) {
 
-		if (year % 100) {
-			if (year % 400)
+			if (year % 100 == 0) {
+
+				if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+					cout << "There are 31 days in this month." << endl;
+				}
+				else if (month == 4 || month == 6 || month == 9 || month == 11) {
+					cout << "There are 30 days in this month." << endl;
+				}
+				else if (month == 2) {
+					cout << "There are 28 days in this month." << endl;
+				}
+			
+				if (year % 400 == 0) {
+					if (month == 2) {
+						cout << "There are 29 days in this month." << endl;
+					}
+
+				}
+		}
 		
-
-
+	
 	return 0;
 }
